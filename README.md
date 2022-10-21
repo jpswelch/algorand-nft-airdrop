78d76bb563ec66fb409cc05ca9fc8725936621d2
Jean-Pierre Welch <jpswelch@gmail.com>
2022-10-21 14:54:04 -0400

    added tailwind and simple connect wallet

---
+++ README.md
@@ -0,0 +1,6 @@
+### Purpose:
+
+[Gitcoin Hackathon: Algorand Greenhouse Hack 2](https://gitcoin.co/hackathon/greenhouse?org=algorandfoundation)  
+[Project: Create An Application Using Randomness](https://gitcoin.co/issue/29366)
+
+#### Demo Site:
--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+// import algosdk from 'algosdk';
+import MyHeader from './components/header/header.component';
 
 function App() {
   return (
-    <div className="App">
-      <h1>NFT AirDrop</h1>
+    <div>
+      <MyHeader />
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-5">NFT AirDrop</p>
+      </div>
     </div>
   );
 }
