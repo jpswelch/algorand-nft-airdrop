d04be9365f7d3feb6cdd7e5b7607075cff9443b3
Jordan Greenberg <77jgreenberg77@gmail.com>
2022-10-27 21:50:24 +0100

    adding form for nft creation

---
+++ frontend/src/NftForm.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import {Button,FormControl,InputLabel,Input, FormHelperText } from "@mui/material"
+
+// import { mintRecipeNFT } from "../actions/RecipeUploadActions";
+
+export const NftForm = () => {
+
+  const [photo, setPhotoData] = useState<any>();
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+//   let handleChange = (i, e) => {
+//     let newIngredientsArray = [...IngredientsArray];
+//     newIngredientsArray[i][e.target.name] = e.target.value;
+//     setIngredientsArray(newIngredientsArray);
+//   };
+
+//   let addNewIngredient = () => {
+//     setIngredientsArray([...IngredientsArray, { name: "", quantity: "" }]);
+//   };
+
+//   let removeIngredient = (i) => {
+//     let newIngredientsArray = [...IngredientsArray];
+//     newIngredientsArray.splice(i, 1);
+//     setIngredientsArray(newIngredientsArray);
+//   };
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+   // console.log(title,description,photo)
+    // const account = accounts[0];
+    // let success = await mintRecipeNFT(
+    //   title,
+    //   description,
+    //   photo,
+    //   account
+    // );
+    // if (success) {
+    //   alert("Minting is complete!");
+    // } else {
+    //   alert("Minting failed! You have already created your NFT for this week!");
+    // }
+  };
+  return (
+    <div className="RecipeUpload center">
+      <h1>Upload your recipe here!</h1>
+            <FormControl>
+                <InputLabel >Title</InputLabel>
+                <Input id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <FormControl/>
+                <FormControl>
+
+                <InputLabel >Description</InputLabel>
+                <Input id="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                </FormControl>
+                <FormControl>
+                <Input
+                    type="file"
+                    onChange={(e) => setPhotoData(e.target.files[0])}
+                    hidden
+                />
+                </FormControl>
+            </FormControl>    
+       
+        <div>
+          <Button onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+    </div>
+  );
+};
