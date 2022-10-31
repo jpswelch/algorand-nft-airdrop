ffc2859e6c4f9fba3b5ef8c02397e7cef53e1714
Jean-Pierre Welch <jpswelch@gmail.com>
2022-10-31 10:55:31 -0400

    added db feature for recording nfts to be donated.

---
+++ frontend/src/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCk1IoE62dni9GHAy6nwJ778VUCevRtpBk',
+  authDomain: 'algorado-rnd-nft-airdrop.firebaseapp.com',
+  databaseURL: 'https://algorado-rnd-nft-airdrop-default-rtdb.firebaseio.com',
+  projectId: 'algorado-rnd-nft-airdrop',
+  storageBucket: 'algorado-rnd-nft-airdrop.appspot.com',
+  messagingSenderId: '765821423122',
+  appId: '1:765821423122:web:d7995b5e59a388be2912cb',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
